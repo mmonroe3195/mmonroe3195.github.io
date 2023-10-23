a6979cf8b8a2b2dafd113be5dfc714ad3fae1a3f
charlotte-whiting <hamilton2023@cocvac.org>
2023-10-23 16:50:04 -0400

    add logo

--- src/app/components/Navigation/navbar/index.tsx
+++ src/app/components/Navigation/navbar/index.tsx
@@ -6,9 +6,11 @@ const Navbar = () => {
     <>
       <div className="w-full h-20 bg-green-950 sticky top-0">
         <div className="flex justify-between items-center h-full">
-          <Link href="/about">
-            <div>about</div>
+          <Link href="https://www.cocvac.org/">
+            <img src="logo.png" width={50} height={50} />
           </Link>
+          <Link href="/">home</Link>
+          <Link href="/about">about</Link>
         </div>
       </div>
     </>
