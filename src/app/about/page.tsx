75acf0ec624b277de1efab566a3bdfe667f56a74
Madison Monroe <67032501+mmonroe3195@users.noreply.github.com>
2023-10-30 15:24:15 -0400

    about madison more detail

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,12 @@ export default function about() {
       <div>We are 3 seniors at Hamilton College!</div>
       <br/>
       <h2>Madison</h2>
-      <div>Hi, I am a senior computer science major at Hamilton College. I am interested in full-stack development and will be moving to Portland, Oregon upon graduation to work as a software engineer at Nike. At school, you can typically find me climbing at the school’s rock wall, holed up in the science center coding, or cycling around the farms nearby Hamilton’s campus. 
+      <div>Hi, I am a senior computer science major at Hamilton College. I am interested in 
+        full-stack development and will be moving to Portland, Oregon upon graduation to work 
+        as a software engineer at Nike. My favorite coding language is Python and outside of 
+        computer science classes, I enjoy talking environmental studies classes. 
+        At school, you can typically find me climbing at the school’s rock wall, holed up in 
+        the science center coding, or cycling around the farms nearby Hamilton’s campus.  
 </div>
     </main>
   );
