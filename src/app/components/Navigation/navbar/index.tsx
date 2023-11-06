2580c844d75e2fcf1fa008dba1ef28ced2409081
charlotte-whiting <hamilton2023@cocvac.org>
2023-11-06 17:00:49 -0500

    alt text for logo

--- src/app/components/Navigation/navbar/index.tsx
+++ src/app/components/Navigation/navbar/index.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="w-full h-20 bg-green-950 sticky top-0">
         <div className="flex items-center h-full">
           <Link className="ml-10" href="https://www.cocvac.org/">
-            <img src="logo.png" width={50} height={50} />
+            <img src="logo.png" width={50} height={50} alt="COCVAC logo" />
           </Link>
           <Link className="ml-12" href="/">
             Home
