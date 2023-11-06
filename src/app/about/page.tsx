cfb79817f5a6d021752406434e71711bd708a76a
charlotte-whiting <hamilton2023@cocvac.org>
2023-11-06 16:59:05 -0500

    finish about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ export default function about() {
       <h1>About the Developers!</h1>
       <div>We are 3 seniors at Hamilton College!</div>
       <br />
-      <h2>Madison</h2>
+      <h2 className="text-2xl">Madison</h2>
       <div>
         Hi, I am a senior computer science major at Hamilton College. I am
         interested in full-stack development and will be moving to Portland,
@@ -16,19 +16,27 @@ export default function about() {
         Hamilton&apos;s campus.
       </div>
       <br />
-      <h2>Sydney</h2>
+      <h2 className="text-2xl">Sydney</h2>
       <div>
-        Hi, I&apos;m also senior computer science major at Hamilton College, and
-        I minor in Africana Studies. I grew up in central New Jersey. I am
-        interested in doing UX Design or software engineering after I graduate.
-        At Hamilton, I&apos;ve enjoyed taking introductory classes to explore
-        the other departments. Some activities I enjoy are playing cello,
-        swimming, and playing pickleball. I also love trying new sports and
-        activities.
+        Hi, I&apos;m also a senior computer science major at Hamilton College,
+        and I am a minor in Africana Studies. I grew up in central New Jersey. I
+        am interested in doing UX Design or software engineering after I
+        graduate. At Hamilton, I&apos;ve enjoyed taking introductory classes to
+        explore the other departments. Some activities I enjoy are playing
+        cello, swimming, and playing pickleball. I also love trying new sports
+        and activities.
       </div>
       <br />
-      <h2>Charlotte</h2>
-      <div>Hi!</div>
+      <h2 className="text-2xl">Charlotte</h2>
+      <div>
+        Hi! I am also a senior computer science major at Hamilton College, and
+        I&apos;m a minor in History. I am from Northern California, and I&apos;m
+        looking forward to working as a software engineer in San Francisco after
+        graduation. I enjoy reading and hiking, and I love learning new
+        languages as well. I&apos;m very interested in software engineering and
+        database design, and outside of computer science I enjoy taking history
+        and philosophy courses.
+      </div>
     </main>
   );
 }
