e5edbf9e25ffcd29c0e620550620baa45ad1e5f1
charlotte-whiting <hamilton2023@cocvac.org>
2023-11-09 11:36:06 -0500

    add heat map report separately

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -10,6 +10,14 @@ export default function Report() {
         src="https://app.powerbi.com/view?r=eyJrIjoiNDI4N2NiNzgtNjY2ZS00Y2E1LThkZWUtYmU4YzU0NzJkNmQzIiwidCI6ImY5ZWE3ZjNlLWRlMjgtNDZkMS1iNjNkLTkxZDMwODZmZmY5MSIsImMiOjN9"
       ></iframe>
       <br></br>
+      <div>Heat Map of where different calls occured:</div>
+      <iframe
+        className="m-1"
+        title="Heat Map"
+        width="1140"
+        height="541.25"
+        src="https://app.powerbi.com/view?r=eyJrIjoiNjkwNzA2NTYtMTBlZC00ODNjLTlkMzktODQzNDNjMmE1NWY1IiwidCI6ImY5ZWE3ZjNlLWRlMjgtNDZkMS1iNjNkLTkxZDMwODZmZmY5MSIsImMiOjN9"
+      ></iframe>
       <div>Charts of different types of calls:</div>
       <iframe
         className="m-1"
