5cf9628501171058e9774f4faa58e63d63e77c54
charlotte-whiting <hamilton2023@cocvac.org>
2023-11-28 15:36:51 -0500

    Update index.tsx

--- src/app/components/Navigation/navbar/index.tsx
+++ src/app/components/Navigation/navbar/index.tsx
@@ -49,7 +49,7 @@ const Navbar = () => {
       <div className="w-full h-20 bg-green-950 sticky top-0">
         <div className="flex items-center h-full text-yellow-500">
           <Link className="ml-10" href="https://www.cocvac.org/">
-            <img src="/logo.png" width={50} height={50} alt="COCVAC logo" />
+            <img src="logo.png" width={50} height={50} alt="COCVAC logo" />
           </Link>
           <Link className="ml-12 hover:text-yellow-200" href="/">
             Home
