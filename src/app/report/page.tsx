ad1413ace8431acee7d27a49804c01d08c9545bf
Madison Monroe <67032501+mmonroe3195@users.noreply.github.com>
2023-12-06 19:35:24 -0500

    Adding in iframes set up at cocvac

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -3,25 +3,15 @@ export default function Report() {
     <div className="flex min-h-screen flex-col items-center justify-between p-24 w-10/12 m-auto">
       <div className="text-center">
         <div className="pb-5 font-bold text-xl font-bold">
-          Maps of where different calls occurred:
+          Table of where different calls occurred:
         </div>
-        <div>To view the various map tabs, click the arrows below.</div>
-        <iframe
-          title="Map Report"
-          width="1140"
-          height="541.25"
-          src="https://app.powerbi.com/view?r=eyJrIjoiNDZiMWQ2ZjAtMzI1Ny00NzlkLWEwYjAtNTFmZTQwNDRjNWUxIiwidCI6ImY5ZWE3ZjNlLWRlMjgtNDZkMS1iNjNkLTkxZDMwODZmZmY5MSIsImMiOjN9"
-        ></iframe>
+         <iframe title="Report Section" width="1140" height="541.25" src="https://app.powerbi.com/view?r=eyJrIjoiYjYzNmYxMzgtMGFmZC00ODhmLWFmNjEtYTI1ODU2MGNjNjk5IiwidCI6ImY5ZWE3ZjNlLWRlMjgtNDZkMS1iNjNkLTkxZDMwODZmZmY5MSIsImMiOjN9"></iframe>
       </div>
       <div className="pt-5 text-center">
         <hr className="background-color: bg-black h-0.5" />
-        <div className="py-5 font-bold text-xl">Chart of 911 Call Data:</div>
-        <iframe
-          title="Report Section"
-          width="1140"
-          height="541.25"
-          src="https://app.powerbi.com/view?r=eyJrIjoiZDhlYjdmZTEtNGVkYy00NTYzLWEyYmMtM2Q2ZmExMzZkZGIyIiwidCI6ImY5ZWE3ZjNlLWRlMjgtNDZkMS1iNjNkLTkxZDMwODZmZmY5MSIsImMiOjN9"
-        ></iframe>
+        <div className="py-5 font-bold text-xl">Map of 911 Call Data:</div>
+        <div>To view the various map tabs, click the arrows below.</div>
+        <iframe title="AT_COCVAC_Maps" width="1140" height="541.25" src="https://app.powerbi.com/view?r=eyJrIjoiN2RiZWM4NGEtOTZhNS00NTE4LWJmNTUtZmEzM2U5YjBhZjAyIiwidCI6ImY5ZWE3ZjNlLWRlMjgtNDZkMS1iNjNkLTkxZDMwODZmZmY5MSIsImMiOjN9&pageName=ReportSectiona553383e314cb81e8ea9"></iframe>
       </div>
       <div className="pt-5 text-center">
         <hr className="background-color: bg-black h-0.5" />
